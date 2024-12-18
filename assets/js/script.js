f9ed772172cff0f43d7a4c4e304f971ce79f3d85
Evans PARFAIT <fani.prft@gmail.com>
2024-12-18 11:40:47 +0100

    Ajout initial des fichiers sur la branche Evans

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     listItems.forEach((item, index) => {
         item.addEventListener("click", (event) => {
-            event.preventDefault(); // Empêche le rechargement de la page
+            event.preventDefault();
 
             if (index === 2) {
-                // Affiche une popup de confirmation
                 const userConfirmed = window.confirm(
                     "Voulez-vous être redirigé vers un lien externe ?"
                 );
                 if (userConfirmed) {
-                    // Redirige l'utilisateur vers le lien contenu dans le <a>
                     const link = item.querySelector("a").href;
                     window.location.href = link;
                 }
-                return; // Ne pas continuer pour le 3ᵉ élément
+                return;
             }
 
-            // Supprime la classe 'active' de tous les éléments
             listItems.forEach(li => li.classList.remove("active"));
 
-            // Ajoute la classe 'active' à l'élément cliqué
             item.classList.add("active");
         });
     });
@@ -33,36 +29,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const nextBtn = document.querySelector(".next-btn");
     const backBtn = document.querySelector(".back-btn");
 
-    // Passer à l'étape suivante
     nextBtn.addEventListener("click", () => {
-        step1.classList.add("left-out"); // Glisse vers la gauche
+        step1.classList.add("left-out");
         step1.classList.remove("active");
 
-        // Ajouter la classe "entering" pour l'animation d'entrée
         step2.classList.add("entering");
         step2.classList.remove("hidden");
 
-        // Attendre la fin de la transition pour activer le deuxième container
         setTimeout(() => {
             step2.classList.remove("entering");
             step2.classList.add("active");
-        }, 50); // Activer l'animation après un petit délai
+        }, 50);
     });
 
-    // Retour à l'étape précédente
     backBtn.addEventListener("click", (e) => {
         e.preventDefault();
         step2.classList.remove("active");
         
-        // Attendre la fin de l'animation avant de faire apparaître la première div
         setTimeout(() => {
             step2.classList.add("hidden");
             step1.classList.remove("hidden");
             setTimeout(() => {
                 step1.classList.remove("left-out");
                 step1.classList.add("active");
-            }, 50); // Activer l'animation après un petit délai
-        }, 500); // Durée de la transition définie en CSS
+            }, 50); 
+        }, 500);
     });
 });
 
